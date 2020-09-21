c3833f1af218619e6ed37c09418c965ac0fff63e
yasnovskiy <andrey5.1997@mail.ru>
2020-09-21 14:17:15 +0500

    Пытался сделать таблицу победителей

---
+++ js/stat.js
@@ -0,0 +1,66 @@
+'use strict';
+
+//Облако (шарина,высота и место нахождение)
+var CLOUD_WIDTH = 420;
+var CLOUD_HEIGHT = 270;
+var CLOUD_X = 100;
+var CLOUD_Y = 10;
+var GAP = 10;
+// var FONT_GAP = 15;
+var TEXT_WIDTH = 50;
+var BAR_WIDTH = 40;
+// var barHeight = CLOUD_HEIGHT - GAP - TEXT_WIDTH - GAP;
+
+var renderCloud = function(ctx, x, y, color) {
+  ctx.fillStyle = color;
+  ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
+};
+
+var getMaxElement = function (arr) {
+  var maxElement = arr[0];
+
+  for (var i = 1; i < arr.length; i++) {
+    if (arr[i] > maxElement) {
+      maxElement = arr[i];
+    }
+  }
+
+  return maxElement;
+};
+
+window.renderStatistics = function (ctx, players, times) {
+  renderCloud(
+      ctx,
+      CLOUD_X + GAP,
+      CLOUD_Y + GAP,
+      'rgba(0, 0, 0, 0.3)'
+  );
+  renderCloud(
+      ctx,
+      CLOUD_X,
+      CLOUD_Y,
+      '#fff'
+  );
+
+  ctx.fillStyle = '#000';
+  ctx.font = '16px Tahoma';
+  ctx.textBaseline = 'hanging';
+  ctx.fillText('Ура вы победили!', 120, 30);
+  ctx.fillText('Список результатов:', 120, 50);
+
+  // var maxTime = getMaxElement(times);
+  //
+  // for (var i = 0; i < players.length; i++) {
+  //   // ctx.fillText(
+  //   //   players[i],
+  //   //   CLOUD_X + GAP,
+  //   //   CLOUD_Y + GAP + FONT_GAP + (GAP + BAR_HEIGHT) * i
+  //   // );
+  //   ctx.fillRect(
+  //     CLOUD_X + GAP + TEXT_WIDTH,
+  //     CLOUD_Y + GAP + (GAP + BAR_WIDTH) * i,
+  //     (barHeight * times[i]) / maxTime,
+  //     BAR_WIDTH
+  //   );
+  // }
+};
